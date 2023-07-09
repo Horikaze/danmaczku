44ce005068360c48601a36b6f0914ff4c15d3474
Horikaze <adrianyt659@gmail.com>
2023-07-09 18:47:29 +0200

    api update

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import Users from "./components/Users/Users";
 export default async function Home() {
   return (
     <div className="flex mt-5 flex-col place-items-center 2xl:grid 2xl:grid-cols-2 gap-5 justify-center drop-shadow-md">
-      <RecetReplays />
-      <Users />
+      {/* <RecetReplays />
+      <Users /> */}
     </div>
   );
 }
