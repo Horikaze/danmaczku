38183bd3529351d96305a28298be9319353dc655
Horikaze <adrianyt659@gmail.com>
2023-07-18 16:53:17 +0200

    fixed favoriteGame change bug

---
+++ app/utils/scoreSystem.ts
@@ -0,0 +1,35 @@
+export const calculateScore = (game:number, score:number)=>{
+    switch (game) {
+        case 6:
+            
+          return "eosd";
+        case 7:
+          return "pcb";
+        case 8:
+          return "in";
+        case 9:
+          return "pofv";
+        case 10:
+          return "mof";
+        case 11:
+          return "sa";
+        case 12:
+          return "ufo";
+        case 128:
+          return "gfw";
+        case 13:
+          return "td";
+        case 14:
+          return "ddc";
+        case 15:
+          return "lolk";
+        case 16:
+          return "hsifs";
+        case 17:
+          return "wbawc";
+        case 18:
+          return "um";
+        default:
+          return "";
+      }
+}
